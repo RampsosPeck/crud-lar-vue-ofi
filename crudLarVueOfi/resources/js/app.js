71479f8da868142a3878fe6dca8cbe8a631ccfd7
RampsosPeck <peralta.jorge.uatf@gmail.com>
2019-03-15 17:29:55 -0400

    Terminando CRUD users

--- crudLarVueOfi/resources/js/app.js
+++ crudLarVueOfi/resources/js/app.js
@@ -8,6 +8,65 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import moment from 'moment';
+import { Form, HasError, AlertError } from 'vform'
+
+//Sweet alet2
+import swal from 'sweetalert2'
+window.swal = swal;
+
+const toast = swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+});
+
+window.toast = toast;
+
+
+window.Form = Form;
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+// configuracion de vue progress bar
+import VueProgressBar from 'vue-progressbar'
+Vue.use(VueProgressBar, {
+  color: 'rgb(143, 255, 199)',
+  failedColor: 'red',
+  height: '2px'
+})
+
+let routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default }, 
+    { path: '/users', component: require('./components/Users.vue').default }, 
+    { path: '/profile', component: require('./components/Profile.vue').default }
+  ]
+
+
+const router = new VueRouter({
+	mode: 'history',
+	routes
+})
+
+
+Vue.filter('upText', function(text){
+	//return text.toUpperCase();
+	return text.charAt(0).toUpperCase() + text.slice(1);
+
+});
+
+
+Vue.filter('myDate', function(created){
+	return moment(created).locale("es").format('MMMM Do YYYY');
+});
+
+ 
+window.Fire = new Vue();
+
 
 /**
  * The following block of code may be used to automatically register your
@@ -29,5 +88,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
